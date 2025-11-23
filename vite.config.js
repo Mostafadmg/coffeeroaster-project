906e9a7cfdc0c1ed2803206252c1d050ac5066b5
Mostafadmg <mostafa.damghani.md@gmail.com>
2025-11-23 22:55:55 +0000

    Add about and plan pages to Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  base: '/coffeeroaster-project/',
+  base: "/coffeeroaster-project/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+        plan: resolve(__dirname, "starter-code/plan.html"),
+      },
+    },
+  },
   server: {
     open: true, // Opens browser automatically
   },
